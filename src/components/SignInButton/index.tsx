f5b56a3f3969bb801743550b7e30b96587df4c6a
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2021-06-18 14:06:24 -0300

    wip

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButton}>
+  return session ? (
+    <button
+      type='button'
+      onClick={() => signOut()}
+      className={styles.signInButton}
+    >
       <FaGithub color='#04d361' />
-      fagner rodrigues
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button
+      type='button'
+      onClick={() => signIn('github')}
+      className={styles.signInButton}
+    >
       <FaGithub color='#eba417' />
       Sign in with Github
     </button>
